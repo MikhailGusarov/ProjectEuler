a8f64f764f56c13b5961086b011ce27a9aff719e
Mikhail Gusarov <vgg639@gmail.com>
2019-10-03 12:18:41 +0300

    Начало задачи 26

---
+++ Problem 26.py
@@ -0,0 +1,13 @@
+from decimal import Decimal
+arr = []
+long = 28
+
+for i in range(2,1000):
+    x = str(Decimal(1)/i)[2:]
+    if len(x) = long:
+        
+    arr.append(str(Decimal(1)/i)[2:])
+    
+
+
+
