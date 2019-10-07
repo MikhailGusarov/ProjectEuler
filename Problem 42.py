a17ab441a9a266b76d81bae77d2c138eae7a9308
MikhailGusarov <vgg639@gmail.com>
2019-10-07 18:52:55 +0300

    Задача 42

--- Problem 42.py
+++ Problem 42.py
@@ -1,12 +1,14 @@
 letters = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-word = 'SKY'
-count =0
-arr_qty = [i/2*(i+1) for i in range(1,10000)]
+with open('Problem 42 words.txt') as data:
+    arr = data.readline().replace('"', '').split(',')
+count = 0
+arr_qty = [int(i/2*(i+1)) for i in range(1, 1000)]
 
-count_word = 0
-for i in word:
-	count_word += letters.index(i)+1
-if count_word in arr_qty:
-	count +=1
-	
+for word in arr:
+    count_word = 0
+    for j in word:
+        count_word += (letters.index(j)+1)
+    if count_word in arr_qty:
+        count += 1
+
 print(count)
