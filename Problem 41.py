ff346677c492f68e159eab782cb9565fde6fd578
Mikhail Gusarov <vgg639@gmail.com>
2019-10-07 16:55:52 +0300

    Задача 41 середина

--- Problem 41.py
+++ Problem 41.py
@@ -4,7 +4,9 @@ y = '123'
 for i in range(4,10):
     y += str(i)
     x += list(itertools.permutations(y))
-
+x = [int(''.join(i)) for i in x if int(''.join(i)) % 2 != 0 ]
+x.sort()
+x.reverse()
 simple_arr = [2]
 
 for i in range(3,1000000, 2):
@@ -13,4 +15,11 @@ for i in range(3,1000000, 2):
             break
         if j ** 2 > i:
             simple_arr.append(i)
+            count = len(x)
+            for k in range(count):
+                if k >= len(x):
+                    break
+                if x[k] % i == 0:
+                    x.pop(k)
             break
+print(max(x),len(x))
