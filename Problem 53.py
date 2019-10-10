a8305161ee96b386b6121899b50c368f67379546
MikhailGusarov <vgg639@gmail.com>
2019-10-10 21:24:34 +0300

    Задача 53

---
+++ Problem 53.py
@@ -0,0 +1,7 @@
+from math import factorial
+count = 0
+for n in range(1, 101):
+    for r in range(0, n + 1):
+        if factorial(n) / (factorial(r) * factorial(n - r)) > 1000000:
+            count += 1
+print(count)
