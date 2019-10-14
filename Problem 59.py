7ffea2ba99bc794d7a23797a018a16c45d5b6d4d
MikhailGusarov <vgg639@gmail.com>
2019-10-14 20:42:12 +0300

    Задача 59

--- Problem 59.py
+++ Problem 59.py
@@ -1,14 +1,18 @@
+def encrypt(message, key):
+    encrypted_message = []
+    for i in range(len(message)):
+        encrypted_message.append(chr(message[i] ^ key[i % len(key)]))
+    return encrypted_message
+
+
 with open('Problem 59 cipher.txt') as data:
     arr = data.readline().split(',')
 arr = [int(i) for i in arr]
-
-
-
-for i in range(97,123):
-    for j in range(97,123):
-        for z in range(97,123):
-            arr_key = [i, j, z] * int((len(arr) / 3))
-            res_step = ''.join([chr(arr[k] ^ arr_key[k]) for k in range(len(arr))])
-            if res_step[0] == '(':
-                print(res_step)
-                
+for i in range(ord('a'), ord('z') + 1):
+    for j in range(ord('a'), ord('z') + 1):
+        z = 112
+        res = ''.join(encrypt(arr, [i, j, z]))
+        if '$' not in res and '}' not in res and '#' not in res and '|' not in res:
+            if '%' not in res:
+                res_ascii = sum([ord(k) for k in (encrypt(arr, [i, j, z]))])
+                print(res_ascii, [chr(i), chr(j), chr(z)], res)
