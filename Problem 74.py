dbb0edbbe0a84a5fd3df726670fc34152ec851c0
MikhailGusarov <vgg639@gmail.com>
2019-10-19 21:35:41 +0300

    Задача 74

--- Problem 74.py
+++ Problem 74.py
@@ -3,7 +3,7 @@ dict_break = {169: 3, 363601: 3, 1454: 3, 871: 2, 45361: 2, 872: 2, 45362: 2, 14
 count_res = 0
 
 for i in range(70, 1000000):
-    count = 0
+    count = 1
     x = sum([math.factorial(int(j)) for j in str(i)])
     while x not in dict_break.keys():
         # print(x)
