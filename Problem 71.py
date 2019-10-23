a06876842c47cb0147f3b779e6ceb9eb626efdab
MikhailGusarov <vgg639@gmail.com>
2019-10-23 21:49:28 +0300

    Задача 71

---
+++ Problem 71.py
@@ -0,0 +1,17 @@
+
+num = 3
+diff = 7
+
+max_num = 1000000
+
+res_num = max_num // diff * num
+res_diff = max_num // diff * diff
+
+
+res_step_num = res_num - 1
+res_step_diff = res_diff
+
+print(res_step_num)
+
+
+
